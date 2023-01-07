51c6a16d67baf7dd508525746afa9a5a468f2e8f
HazelWang0 <hugowell@163.com>
2023-01-07 19:40:14 +0800

    update

---
+++ 27.移除元素.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=27 lang=python3
+#
+# [27] 移除元素
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+# @lc code=end
+
